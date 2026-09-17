ceddacfa7e75abdc8f2a014cb5e6825b83858745
Muhammad-Ashfaq1 <mitf19e032@gmail.com>
2026-09-17 23:12:28 +0500

    feat: add GitHub Actions deployment workflow and permission sync console command

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('app:sync-permissions', function () {
+    $this->info('Syncing roles and permissions...');
+    $this->call('db:seed', [
+        '--class' => 'RolePermissionSeeder',
+        '--force' => true,
+    ]);
+    $this->info('Roles and permissions synced.');
+})->purpose('Sync application roles and permissions');
